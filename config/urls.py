272dbf9d851477a261c5801e3e2cd07f6e1c1e35
andriytok <andriy.tokovyy@gmail.com>
2026-08-20 20:45:00 +0300

    base config completed

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from config.views import health_check
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/users/', include('apps.users.urls')),
-    path('api/v1/cars/', include('apps.cars.urls')),
+    path('admin', admin.site.urls),
+    path('health', health_check, name='health-check'),
+    path('api/v1/users', include('apps.users.urls')),
+    path('api/v1/cars', include('apps.cars.urls')),
 ]
 
 if settings.DEBUG:
